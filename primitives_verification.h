fe0b37aa2a0eb20f19efb9b19fcac4368044b7a1
durvaal <paulo.lima@zrp.com.br>
2019-11-18 18:24:13 -0300

    Added .h files

---
+++ primitives_verification.h
@@ -0,0 +1,65 @@
+void checkPrimitive(char *name, char *arguments[100]){
+    if (strcmp(name, "image") == 0){
+        image(atoi(arguments[0]), atoi(arguments[1]));
+    }
+    if (strcmp(name, "color") == 0){
+        color(atoi(arguments[0]), atoi(arguments[1]), atoi(arguments[2]));
+    }
+    if (strcmp(name, "clear") == 0){
+        clear(atoi(arguments[0]), atoi(arguments[1]), atoi(arguments[2]));
+    }
+    if (strcmp(name, "rect") == 0){
+
+    }
+    if (strcmp(name, "circle") == 0){
+        cicle(atoi(arguments[0]), atoi(arguments[1]), atoi(arguments[2]));
+    }
+    if (strcmp(name, "polygon") == 0){
+
+    }
+    if (strcmp(name, "fill") == 0){
+
+    }
+    if (strcmp(name, "save") == 0){
+        save(arguments[0]);
+    }
+    if (strcmp(name, "line") == 0){
+        line(atoi(arguments[0]), atoi(arguments[1]), atoi(arguments[2]), atoi(arguments[3]));
+    }
+    if (strcmp(name, "open") == 0){
+        open(arguments[0]);
+    }
+}
+
+void extractArgumentsPrimitive(char *primitiveLine) {
+	char delimiter[] = " ";
+	char *primitiveName = strtok(primitiveLine, delimiter);
+	char *primitiveArgument = strtok(NULL, delimiter);
+    char *primitiveArguments[100];
+    int i = -1;
+
+    while (primitiveArgument != NULL) {
+        i++;
+        primitiveArguments[i] = removeBreakLine(primitiveArgument);
+		primitiveArgument = strtok(NULL, delimiter);
+	}
+
+    checkPrimitive(primitiveName, primitiveArguments);
+}
+
+void readPrimitesFile() {
+    char primitiveLine[100];
+  
+    FILE *arq;
+    // Abre um arquivo TEXTO para LEITURA
+    arq = fopen("primitives", "r");
+    // Se houve erro na abertura
+    if (arq == NULL) {
+        printf("Problemas na abertura do arquivo\n");
+        exit(0);
+    }
+    while (fgets(primitiveLine, 100, arq) != NULL){
+        extractArgumentsPrimitive(primitiveLine);
+    }
+    fclose(arq);
+}
---
+++ structs.h
@@ -0,0 +1,21 @@
+// struct dos pixels em RGB;
+typedef struct{
+	unsigned int r, g, b;
+} PPMPixel;
+
+// struct da image PPM contendo a struct de pixels em RGB;
+typedef struct {
+     int x, y, maxRGBRange;
+     char *fileName, *typeEncoding;
+     PPMPixel *matrizDePixels;
+} PPMImage;
+
+// struct que define a cor do pincel atual
+typedef struct {
+    int r, g, b;
+} PPMColor;
+
+
+// struct do objeto PPM manipulado em tempo de execução
+PPMImage *imageGlobal;
+PPMColor *ppmColor;
